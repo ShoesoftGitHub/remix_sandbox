11bf1cdecce494afba922d016e49d7a0bbc07fe9
ShoesoftGitHub <social@shoe-soft.com>
2023-06-05 08:45:07 +0200

    Added feature token

---
+++ contracts/feature_token/FeatureToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+
+/**
+ * @dev This is a sample contract that combines a lot of features.
+ * This contract and its dependencies serve to demonstrate how those features work
+ * and how they are implemented.
+ * Features include:
+ * - ERC20 and DEX / Pancakeswap
+ *      - Adding and removing liquidity from a DEX
+ *      - Selling and buying tokens from a DEX
+ *      - Enabling and disabling those functions
+ * - Context
+ * - Ownable
+ * - Pausable
+ * - Whitelist / Blacklist
+ * - Mint and Burn
+ * - Taxes (also add to DEX LP)
+ * - Dividends (also in combination with a DEX)
+ * - Reflections
+ * - Timers
+ * - Vesting
+ * - Payment Splitting?
+ * - Voting
+ * - Roles
+ * - NFTs
+ * - Blind Auction
+ * - Permission to third parties
+ * - Flash Minting
+ * - Snapshots
+ * - Upgradable
+ * - Cross chain
+ */
+contract FeatureToken {
+}
