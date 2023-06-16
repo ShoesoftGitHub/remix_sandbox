4326b4b1f0bb5d4a0b327d283b2f335f9952df6a
ShoesoftGitHub <social@shoe-soft.com>
2023-06-16 11:59:22 +0200

    changed payment js files

---
+++ scripts/basics/payment/verify_message.js
@@ -0,0 +1,26 @@
+// The following is a way to verify that messages are correct, this way
+// the recipient can verify that it receives the correct amount and the signer
+// was the sender and the contract is the right one.
+// It uses the ethereumjs-util library.
+
+// this mimics the prefixing behavior of the eth_sign JSON-RPC method.
+function prefixed(hash) {
+    return ethereumjs.ABI.soliditySHA3(
+        ["string", "bytes32"],
+        ["\x19Ethereum Signed Message:\n32", hash]
+    );
+}
+
+function recoverSigner(message, signature) {
+    var split = ethereumjs.Util.fromRpcSig(signature);
+    var publicKey = ethereumjs.Util.ecrecover(message, split.v, split.r, split.s);
+    var signer = ethereumjs.Util.pubToAddress(publicKey).toString("hex");
+    return signer;
+}
+
+function isValidSignature(contractAddress, amount, signature, expectedSigner) {
+    var message = prefixed(constructPaymentMessage(contractAddress, amount));
+    var signer = recoverSigner(message, signature);
+    return signer.toLowerCase() ==
+        ethereumjs.Util.stripHexPrefix(expectedSigner).toLowerCase();
+}
